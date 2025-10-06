2e2b21b9dade47a0f54d633188321ec76e87188e
FARABI12344 <farabiehan4@gmail.com>
2025-10-06 14:18:34 +0600

    Added package.json

--- backend/db.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-from decouple import config
-
-MONGO_URL = config("MONGO_URL")  # Reads from .env
-DB_NAME = config("DB_NAME", default="notez_fun_db")
-
-client = MongoClient(MONGO_URL)
-db = client[DB_NAME]
